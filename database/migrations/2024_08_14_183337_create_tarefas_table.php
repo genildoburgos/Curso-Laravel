4f8d890abfeab68d4ee23950faea742dd8d8bf1a
genildoburgos <gnldburgos@gmail.com>
2024-08-14 15:36:13 -0300

    Mer: Adicionado Logica De Atividade e Tarefa

---
+++ database/migrations/2024_08_14_183337_create_tarefas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tarefas', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nome');
+            $table->boolean('flag_concluida');
+
+            $table->foreignId('atividade_id')->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tarefas');
+    }
+};
